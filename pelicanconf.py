2bc6559b8df129e95098cfb5eecf61f8ef73df4e
Robert Kyle <revspooner@gmail.com>
2014-12-29 16:55:09 +0000

    configure blog with python notebook

--- pelicanconf.py
+++ pelicanconf.py
@@ -36,3 +36,9 @@ DEFAULT_PAGINATION = 10
 
 STATIC_PATHS = ['images', 'extra/CNAME']
 EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
+
+PLUGIN_PATHS = ['plugins']
+#PLUGINS = ['liquid_tags.img', 'liquid_tags.video', 'liquid_tags.youtube', 'liquid_tags.vimeo', 'liquid_tags.include_code', 'liquid_tags.notebook']
+PLUGINS = ['liquid_tags.notebook']
+
+EXTRA_HEADER = open('_nb_header.html').read().decode('utf-8')
